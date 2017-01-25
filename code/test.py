dc0f8eef8448863370d2223fa0c2987bb94a0d77
649435349 <649435349@qq.com>
2017-01-26 00:49:19 +0800

    01.27 mysql建表

--- code/test.py
+++ code/test.py
@@ -14,13 +14,13 @@ conn = pymysql.connect(host='127.0.0.1', user='root', passwd='fyf!!961004',chars
 cur = conn.cursor()
 cur.execute('use competition')
 with open('user_view.txt') as f:
-    l=f.readlines()
-
-for i in l:
-    tmp=i.split(',')
-    sql='insert into user_view(user_id,shop_id,time_stamp) values (%s,%s,%s)'
-    cur.execute(sql,tmp)
-    conn.commit()
+    i='1'
+    while len(i)!=0
+        i=f.readline()
+        tmp=i.split(',')
+        sql='insert into user_view(user_id,shop_id,time_stamp) values (%s,%s,%s)'
+        cur.execute(sql,tmp)
+        conn.commit()
 
 cur.close()
 conn.close()
